c9eaa540b04dcb03e333fdc2dac7e05386e54e70
QHuyNguyen <quochuynguyen210@gmail.com>
2023-06-12 21:54:21 +1000

    Update ou tf file

--- aws-organization/organization-unit.tf
+++ aws-organization/organization-unit.tf
@@ -1,6 +1,6 @@
-data "aws_organizations_organization" "example" {}
+data "aws_organizations_organization" "org" {}
 
-resource "aws_organizations_organizational_unit" "example" {
-  name      = "example"
-  parent_id = data.aws_organizations_organization.example.roots[0].id
+resource "aws_organizations_organizational_unit" "non_prod" {
+  name      = "Non Prod"
+  parent_id = data.aws_organizations_organization.org.roots.id
 }
