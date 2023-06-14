31350a795e66998697becb55a3647289103e1516
QHuyNguyen <quochuynguyen210@gmail.com>
2023-06-14 20:26:46 +1000

    test7

--- environments/non-prod/ap-southeast-2/organization/main.tf
+++ environments/non-prod/ap-southeast-2/organization/main.tf
@@ -1,4 +1,4 @@
 module "organization" {
     source = "../../../../modules/aws-organization"
-    ou_name      = "bitch"
+    ou_name      = var.name
 }
--- environments/non-prod/ap-southeast-2/organization/module.tfvars
+++ environments/non-prod/ap-southeast-2/organization/module.tfvars
@@ -1,1 +1,1 @@
-#name = "cunt"
+name = "cunt"
