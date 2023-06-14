e2cad0cf824b5a51953ba7723dbaf7562306b5b6
QHuyNguyen <quochuynguyen210@gmail.com>
2023-06-14 20:24:35 +1000

    test6

--- environments/non-prod/ap-southeast-2/organization/provider.tf
+++ environments/non-prod/ap-southeast-2/organization/provider.tf
@@ -1,3 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "arthur-tf-state-bucket"
+    key    = "organization/organization-terraform.tfstate"
+    region = "ap-southeast-2"
+  }
+}
+
 provider "aws" {
   region = "ap-southeast-2"
 }
