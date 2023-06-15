2a055ac0d17c9d159436724d6d0c73ff385b3d5d
QHuyNguyen <quochuynguyen210@gmail.com>
2023-06-15 14:44:17 +1000

    Add tf file for s3 bucket

--- environments/non-prod/ap-southeast-2/s3/provider.tf
+++ environments/non-prod/ap-southeast-2/s3/provider.tf
@@ -5,6 +5,12 @@ terraform {
       version = "~> 4.0"
     }
   }
+
+  backend "s3" {
+    bucket = "arthur-tf-state-bucket"
+    key    = "s3/bucket-terraform.tfstate"
+    region = "ap-southeast-2"
+  }
 }
 
 provider "aws" {
