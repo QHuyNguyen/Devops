ff3a04b8f3951421a0e62ffba052ec1a21cede5d
QHuyNguyen <quochuynguyen210@gmail.com>
2023-06-15 14:13:20 +1000

    Add new bucket

---
+++ environments/non-prod/ap-southeast-2/s3/main.tf
@@ -0,0 +1,5 @@
+module "bucket" {
+  source = "../../../../modules/aws-resource/s3"
+  region = var.region
+  environment = var.environment
+}
--- modules/aws-resource/s3/s3.tf
+++ modules/aws-resource/s3/s3.tf
@@ -1,8 +1,8 @@
 resource "aws_s3_bucket" "tf-state-bucket" {
-  bucket = "arthur-tf-state-bucket"
+  bucket = "arthur-${var.region}-tf-state-bucket"
 
   tags = {
     Name        = "My terraform state bucket"
-    Environment = "Non-Prod"
+    Environment = var.environment
   }
 }
