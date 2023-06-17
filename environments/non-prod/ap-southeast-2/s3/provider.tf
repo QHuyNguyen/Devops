feac44a782580534654052d655d6dfed33856417
QHuyNguyen <quochuynguyen210@gmail.com>
2023-06-17 17:00:05 +1000

    Deploy k8s workflow

--- environments/non-prod/ap-southeast-2/s3/provider.tf
+++ environments/non-prod/ap-southeast-2/s3/provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.74.2"
+      version = "~> 4.0"
     }
   }
 
@@ -14,7 +14,5 @@ terraform {
 }
 
 provider "aws" {
-  profile = "default"
   region = "ap-southeast-2"
-  shared_credentials_file = "~/.aws/config"
 }
