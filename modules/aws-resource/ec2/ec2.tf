6564ad4a4141218a95b2b14dd7f3d3316c7bec5b
QHuyNguyen <quochuynguyen210@gmail.com>
2023-06-19 19:25:16 +1000

    Update ec2 module to deploy instance to custom vpc subnet

--- modules/aws-resource/ec2/ec2.tf
+++ modules/aws-resource/ec2/ec2.tf
@@ -1,3 +1,10 @@
+data "aws_subnets" "k8s_subnet" {
+  filter {
+    name   = "k8s-vpc"
+    values = ["k8s-vpc"]
+  }
+}
+
 data "aws_ami" "ubuntu" {
   most_recent = true
 
@@ -17,6 +24,7 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "worker_node" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
+  subnet_id = data.aws_subnets.k8s_subnet.id
 
   tags = {
     Name = var.name
--- modules/aws-resource/vpc/subnet.tf
+++ modules/aws-resource/vpc/subnet.tf
@@ -1,9 +1,10 @@
 resource "aws_subnet" "name" {
   vpc_id = aws_vpc.k8s_vpc.id
   cidr_block = "10.0.1.0/24"
+  availability_zone = "ap-southeast-2a"
 
   tags = {
-    Name = "Subnet A"
+    Name = "k8s-vpc-subnet-A"
     Owner = "arthur"
   }
 }
