43396fccdeac455d822b2afd509c1274a1bc1ec8
QHuyNguyen <quochuynguyen210@gmail.com>
2023-06-22 19:14:03 +1000

    Add nacl to subnet

--- modules/aws-resource/vpc/nacl.tf
+++ modules/aws-resource/vpc/nacl.tf
@@ -1,31 +1,31 @@
-#resource "aws_network_acl" "k8s_nacl" {
-#  vpc_id = aws_vpc.k8s_vpc.id
-#
-#  egress {
-#    protocol   = "tcp"
-#    rule_no    = 200
-#    action     = "allow"
-#    cidr_block = "0.0.0.0/0"
-#    from_port  = 443
-#    to_port    = 443
-#  }
-#
-#  ingress {
-#    protocol   = "tcp"
-#    rule_no    = 100
-#    action     = "allow"
-#    cidr_block = "0.0.0.0/0"
-#    from_port  = 80
-#    to_port    = 80
-#  }
-#
-#  tags = {
-#    Name = "k8s-subnet-nacl"
-#    Owner = var.owner
-#  }
-#}
-#
-#resource "aws_network_acl_association" "main" {
-#  network_acl_id = aws_network_acl.k8s_nacl.id
-#  subnet_id      = aws_subnet.k8s_subnet.id
-#}
+resource "aws_network_acl" "k8s_nacl" {
+  vpc_id = aws_vpc.k8s_vpc.id
+
+  egress {
+    protocol   = "tcp"
+    rule_no    = 200
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 443
+    to_port    = 443
+  }
+
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 80
+    to_port    = 80
+  }
+
+  tags = {
+    Name = "k8s-subnet-nacl"
+    Owner = var.owner
+  }
+}
+
+resource "aws_network_acl_association" "main" {
+  network_acl_id = aws_network_acl.k8s_nacl.id
+  subnet_id      = aws_subnet.k8s_subnet.id
+}
