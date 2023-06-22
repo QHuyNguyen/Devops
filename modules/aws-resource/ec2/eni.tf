d11ba9be0d6f47b58d9cf8a70cc19cf05cc14024
QHuyNguyen <quochuynguyen210@gmail.com>
2023-06-22 14:05:51 +1000

    Update eni

--- modules/aws-resource/ec2/eni.tf
+++ modules/aws-resource/ec2/eni.tf
@@ -2,11 +2,6 @@ resource "aws_network_interface" "worker_node_eni" {
   subnet_id       = data.aws_subnet.selected_subnet.id
   private_ips     = ["10.0.1.50"]
 
-  attachment {
-    instance     = aws_instance.worker_node.id
-    device_index = 1
-  }
-
   tags = {
     Name = "k8s_primary_network_interface"
     Owner = var.owner
