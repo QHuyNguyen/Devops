91a8363d7fad506e727e3e653b51006f96b2ef97
QHuyNguyen <quochuynguyen210@gmail.com>
2023-06-22 18:35:21 +1000

    fuck

--- modules/aws-resource/vpc/route_table.tf
+++ modules/aws-resource/vpc/route_table.tf
@@ -1,18 +1,18 @@
-resource "aws_route_table" "k8s_custom_rt" {
-  vpc_id = aws_vpc.k8s_vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.k8s_igw.id
-  }
-
-  tags = {
-    Name = "k8s-rt"
-    Owner = var.owner
-  }
-}
-
-resource "aws_main_route_table_association" "a" {
-  vpc_id         = aws_vpc.k8s_vpc.id
-  route_table_id = aws_route_table.k8s_custom_rt.id
-}
+#resource "aws_route_table" "k8s_custom_rt" {
+#  vpc_id = aws_vpc.k8s_vpc.id
+#
+#  route {
+#    cidr_block = "0.0.0.0/0"
+#    gateway_id = aws_internet_gateway.k8s_igw.id
+#  }
+#
+#  tags = {
+#    Name = "k8s-rt"
+#    Owner = var.owner
+#  }
+#}
+#
+#resource "aws_main_route_table_association" "a" {
+#  vpc_id         = aws_vpc.k8s_vpc.id
+#  route_table_id = aws_route_table.k8s_custom_rt.id
+#}
--- modules/aws-resource/vpc/subnet.tf
+++ modules/aws-resource/vpc/subnet.tf
@@ -3,8 +3,6 @@ resource "aws_subnet" "k8s_subnet" {
   cidr_block = "10.0.1.0/24"
   availability_zone = "ap-southeast-2a"
 
-  depends_on = [aws_internet_gateway.k8s_igw]
-
   tags = {
     Name = "k8s-vpc-subnet-A"
     Owner = "arthur"
