4a119c2330cc2a00817c8c75b58b579935257a08
QHuyNguyen <quochuynguyen210@gmail.com>
2023-06-23 15:24:52 +1000

    fix 2

--- environments/non-prod/ap-southeast-2/k8s/main.tf
+++ environments/non-prod/ap-southeast-2/k8s/main.tf
@@ -10,6 +10,7 @@ module "instance" {
   instance_count = var.instance_count
   instance_type = var.instance_type
   instance_name = var.instance_name
+  private_ips = var.private_ips
   owner = var.owner
 #  subnet_id = var.subnet_id
   public_subnet_id = module.vpc.public_subnet_id
