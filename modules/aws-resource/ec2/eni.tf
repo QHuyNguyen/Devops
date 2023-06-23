1a76fb816dddb88994422bfcfc5dcca4c233c386
QHuyNguyen <quochuynguyen210@gmail.com>
2023-06-23 16:24:15 +1000

    fix 10

--- modules/aws-resource/ec2/eni.tf
+++ modules/aws-resource/ec2/eni.tf
@@ -1,7 +1,7 @@
 resource "aws_network_interface" "worker_node_eni" {
   count = var.instance_count
   subnet_id       = var.public_subnet_id
-  private_ips     = element(var.private_ips, count.index)
+  private_ips     = var.private_ips
 
   tags = {
     Name = join("-", ["k8s_primary_network_interface", count.index])
@@ -13,7 +13,7 @@ resource "aws_eip" "worker_node_eip" {
   vpc = true
   count = var.instance_count
   network_interface = aws_network_interface.worker_node_eni[count.index].id
-  associate_with_private_ip = "10.0.1.50"
+  associate_with_private_ip = var.private_ips
   depends_on        = [aws_instance.worker_node]
   tags = {
     Name = join("-", ["k8s_eip", count.index])
