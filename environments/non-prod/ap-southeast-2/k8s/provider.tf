e15b9e7aed610d8c4c82f1e4f4e2b3279c393d55
QHuyNguyen <quochuynguyen210@gmail.com>
2023-06-27 16:05:03 +1000

    update 2

--- environments/non-prod/ap-southeast-2/k8s/provider.tf
+++ environments/non-prod/ap-southeast-2/k8s/provider.tf
@@ -9,10 +9,14 @@ terraform {
   backend "s3" {
     bucket = "arthur-cmd-state-file-bucket"
     key    = "k8s/instance-terraform.tfstate"
+    role_arn = "arn:aws:iam::722141136946:role/arthur-OpenID-role-test"
     region = "ap-southeast-2"
   }
 }
 
 provider "aws" {
   region = "ap-southeast-2"
+  assume_role {
+    role_arn = "arn:aws:iam::722141136946:role/arthur-OpenID-role-test"
+  }
 }
