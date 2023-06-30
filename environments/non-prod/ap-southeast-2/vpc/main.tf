c96df76229b444b4c28accea7be20ffe9f03cf49
QHuyNguyen <quochuynguyen210@gmail.com>
2023-06-30 18:30:48 +1000

    fix 9

--- environments/non-prod/ap-southeast-2/vpc/main.tf
+++ environments/non-prod/ap-southeast-2/vpc/main.tf
@@ -1,6 +1,7 @@
 module "vpc" {
   source = "../../../../modules/aws-resource/vpc"
   vpc_cidr = var.vpc_cidr
+  subnet_count = var.subnet_count
   subnet_cidr = var.subnet_cidr
   vpc_name = var.vpc_name
   owner = var.owner
