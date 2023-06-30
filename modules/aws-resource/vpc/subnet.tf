eadcc0fba8919d38eb45aa84e08e8676b5de79d0
QHuyNguyen <quochuynguyen210@gmail.com>
2023-06-30 17:48:56 +1000

    fix

--- modules/aws-resource/vpc/subnet.tf
+++ modules/aws-resource/vpc/subnet.tf
@@ -1,7 +1,7 @@
 resource "aws_subnet" "k8s_subnet" {
   count = var.subnet_count
   vpc_id = aws_vpc.k8s_vpc.id
-  cidr_block = var.subnet_cidr
+  cidr_block = var.subnet_cidr[count.index]
   availability_zone = "ap-southeast-2a"
 
   tags = {
