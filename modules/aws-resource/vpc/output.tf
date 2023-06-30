f16cc5536466ee8fde674e5781602c995339f44a
QHuyNguyen <quochuynguyen210@gmail.com>
2023-06-30 17:55:55 +1000

    fix 4

--- modules/aws-resource/vpc/output.tf
+++ modules/aws-resource/vpc/output.tf
@@ -1,7 +1,6 @@
-output "public_subnet_id" {
-    count = var.subnet_count
-    value = aws_subnet.k8s_subnet.id[count.index]
-}
+#output "public_subnet_id" {
+#    value = aws_subnet.k8s_subnet.id
+#}
 
 output "k8s_sg_id" {
     value = aws_security_group.allow_SSH_HTTP.id
