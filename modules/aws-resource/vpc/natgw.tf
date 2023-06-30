4719bcbfc993abf683cacc0984dbb2b2cdaf80a3
QHuyNguyen <quochuynguyen210@gmail.com>
2023-06-30 19:20:39 +1000

    fix 3

--- modules/aws-resource/vpc/natgw.tf
+++ modules/aws-resource/vpc/natgw.tf
@@ -1,6 +1,6 @@
 resource "aws_nat_gateway" "nat_gateway" {
   allocation_id = aws_eip.nat_gateway_eip.id
-  subnet_id = aws_subnet.k8s_subnet[1].id
+  subnet_id = aws_subnet.k8s_subnet[0].id
   tags = {
     "Name" = "k8s-arthur-nat-gateway"
   }
