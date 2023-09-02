bd352e0b8508ea8c3f286d975ac7685160fd2b1c
QHuyNguyen <quochuynguyen210@gmail.com>
2023-09-02 15:47:27 +1000

    fix 3

--- components/ec2/variables.tf
+++ components/ec2/variables.tf
@@ -2,16 +2,8 @@ variable "instance_type" {
   type = string
 }
 
-variable "cidr_range" {
-  type = string
-}
-
 variable "instance_name" {
   type = list(string)
-}
-
-variable "vpc_name" {
-  type = string
 }
 
 variable "owner" {
