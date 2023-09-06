ccbcb9ac5469cc5eb56d2ade6bc55a5d132953f3
QHuyNguyen <quochuynguyen210@gmail.com>
2023-09-06 23:09:04 +1000

    fix 12

--- modules/aws-resource/vpc/tier.tf
+++ modules/aws-resource/vpc/tier.tf
@@ -1,8 +1,9 @@
 module "tier" {
   source                     = "./tier"
+  for_each = var.tiers
   availability_zone_suffixes = var.availability_zone_suffixes
   vpc_id                     = aws_vpc.this.id
   subnet_cidr                = var.subnet_cidr
   vpc_name                   = var.vpc_name
-  tiers = var.tiers
+  tiers = each.value
 }
