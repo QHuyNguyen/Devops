ce4b55803fb762a13fcb9f59d6e2da80b6da7feb
QHuyNguyen <quochuynguyen210@gmail.com>
2023-09-09 16:24:12 +1000

    fix 13

--- modules/aws-resource/vpc/natgw.tf
+++ modules/aws-resource/vpc/natgw.tf
@@ -1,7 +1,7 @@
 resource "aws_nat_gateway" "this" {
   #count = length(var.availability_zone_suffixes)
   allocation_id = aws_eip.this.id
-  subnet_id = module.tier["public_subnet"].public_subnet
+  subnet_id = module.tier["public_subnet"].subnet
   tags = {
     "Name" = "devops-nat-gateway"
   }
