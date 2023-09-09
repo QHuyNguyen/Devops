c9f5e893b069cd465fdf26d80104a64891576a46
QHuyNguyen <quochuynguyen210@gmail.com>
2023-09-09 16:20:54 +1000

    fix 12

--- modules/aws-resource/vpc/tier/output.tf
+++ modules/aws-resource/vpc/tier/output.tf
@@ -1,12 +1,8 @@
-output "public_subnet" {
-  value = aws_subnet.this[0].id
-}
-
-output "private_subnet" {
-  value = aws_subnet.this[1].id
-}
-
 output "subnet" {
+  value = aws_subnet.this[2].id
+}
+
+output "subnets" {
   value = aws_subnet.this[*].id
 }
 
