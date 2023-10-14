5242d9f22eeb8db37e1dc0f02f18917145b25e35
QHuyNguyen <quochuynguyen210@gmail.com>
2023-10-14 17:32:16 +1100

    fix 4

--- components/vpc/variables.tf
+++ components/vpc/variables.tf
@@ -19,9 +19,9 @@ variable "subnet_count" {
   default = 1
 }
 
-variable "subnet_name" {
-  type = list(string)
-}
+#variable "subnet_name" {
+#  type = list(string)
+#}
 
 variable "public_cidr_blocks" {
   type = list(string)
