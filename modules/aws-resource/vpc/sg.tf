6471f4f782f50c86a5875afa2f4d654fc60f3cc1
QHuyNguyen <quochuynguyen210@gmail.com>
2023-10-16 15:01:59 +1100

    updated

--- modules/aws-resource/vpc/sg.tf
+++ modules/aws-resource/vpc/sg.tf
@@ -29,6 +29,7 @@ resource "aws_security_group" "allow_SSH_HTTP" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
     from_port   = 0
