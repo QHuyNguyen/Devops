4f2a38054027ff1ae304e49e9e5228b3ef9f325e
QHuyNguyen <quochuynguyen210@gmail.com>
2025-01-27 23:19:33 +0700

    Add route table to public subnet

---
+++ modules/aws-resource/vpc-testing/route_table.tf
@@ -0,0 +1,18 @@
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.this.id
+  }
+
+  tags = {
+    Name  = "${var.vpc_name}-public-rt"
+    Owner = var.owner
+  }
+}
+
+resource "aws_route_table_association" "public_asso" {
+  subnet_id      = module.tier["public_subnet"].subnet_az_a
+  route_table_id = aws_route_table.public_rt.id
+}
