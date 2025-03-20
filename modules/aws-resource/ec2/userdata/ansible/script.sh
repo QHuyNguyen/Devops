78767bc56a702456b24a12de1043e9b262cdc6a4
QHuyNguyen <quochuynguyen210@gmail.com>
2025-03-20 19:16:27 +1100

    Update cred

--- modules/aws-resource/ec2/userdata/ansible/script.sh
+++ modules/aws-resource/ec2/userdata/ansible/script.sh
@@ -5,7 +5,7 @@ sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt install -y ansible curl unzip
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
-sudo rm -rf awscliv2.zip
+#sudo rm -rf awscliv2.zip
 sudo ./aws/install
 sudo aws s3 cp s3://arthur-mantel-ap-southeast-2-bucket/zip/ansible_k8s-master.zip .
 sudo unzip ansible_k8s-master.zip
