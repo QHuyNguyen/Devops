c1f023ca5b555ef2ca07c869d0f14af54e8e24d2
QHuyNguyen <quochuynguyen210@gmail.com>
2025-05-02 02:06:20 +1000

    add function to download keypair

--- modules/aws-resource/ec2/key_pair.tf
+++ modules/aws-resource/ec2/key_pair.tf
@@ -5,6 +5,10 @@ resource "tls_private_key" "ed25519-key" {
 resource "aws_key_pair" "this" {
   key_name   = "cluster-key"
   public_key = trimspace(tls_private_key.ed25519-key.public_key_openssh)
+  
+  provisioner "local-exec" { # Create "myKey.pem" to your computer!!
+    command = "echo '${trimspace(tls_private_key.pk.private_key_pem)}' > ./sshkey-${aws_key_pair.this.key_name}.pem"
+  }
 }
 
 resource "local_sensitive_file" "this" {
