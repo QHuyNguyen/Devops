0829d8f58cf0722440ac0f0bef3b9b198e463ebc
QHuyNguyen <quochuynguyen210@gmail.com>
2025-12-17 19:29:35 +1100

    another fix for vpc

--- modules/aws-resource/vpc/tier.tf
+++ modules/aws-resource/vpc/tier.tf
@@ -1,12 +1,3 @@
-#module "tier" {
-#    source = "./tier"
-#    for_each = var.tiers
-#    vpc_name = var.vpc_name
-#    vpc_id = aws_vpc.this.id
-#    subnet_name = each.value
-#    subnet_cidr = ["sdfd"]
-#    tiers = each.value
-#}
 module "tier" {
   source      = "./tier"
   for_each    = var.tiers
@@ -14,4 +5,5 @@ module "tier" {
   vpc_name    = var.vpc_name
   tiers       = each.value
   subnet_name = each.key
+  subnet_cidr = ""
 }
